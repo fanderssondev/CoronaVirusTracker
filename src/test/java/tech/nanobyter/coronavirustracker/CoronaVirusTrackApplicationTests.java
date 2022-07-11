db6ff5aed70959a071b939e6e0ef7067ffd05de2
nanobyter <sotarfredrik@gmail.com>
2022-07-11 18:01:32 +0200

    Model and service added

--- src/test/java/tech/nanobyter/coronavirustracker/CoronaVirusTrackApplicationTests.java
+++ src/test/java/tech/nanobyter/coronavirustracker/CoronaVirusTrackApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package tech.nanobyter.coronavirustracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CoronaVirusTrackApplicationTests {
 
 	@Test
 	void contextLoads() {
